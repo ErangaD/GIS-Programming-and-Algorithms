db2815e14d8e0ad9a8b1f437d5ddfefb6d488791
ErangaD <erangadulshan.14@cse.mrt.ac.lk>
2018-04-21 23:59:10 +0530

    added colored graph

--- src/viewPolygon.py
+++ src/viewPolygon.py
@@ -7,5 +7,6 @@ plt.figure()
 for shape in sf.shapeRecords():
     x = [i[0] for i in shape.shape.points[:]]
     y = [i[1] for i in shape.shape.points[:]]
+    plt.fill(x,y);
     plt.plot(x,y)
 plt.show()
